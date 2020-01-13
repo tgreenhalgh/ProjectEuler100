e766b81f4541f01c6690400c9e474b94b25348f6
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2020-01-13 15:50:33 -0800

    finishes nthPrime for JavaScript

--- javascript/07 nthPrime.js
+++ javascript/07 nthPrime.js
@@ -1,7 +1,8 @@
-/** @format */
 const isPrime = n => {
   if (n < 2) return false;
-  for (let i = 2; i * i <= n; i++) if (n % i == 0) return false;
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i == 0) return false;
+  }
   return true;
 };
 
